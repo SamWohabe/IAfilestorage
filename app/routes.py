ff775c84f06e87f7e7f0f3952a2fbddbacee981b
SamWohabe <2021swohabe@05095.dwight.edu>
2020-02-06 10:25:32 -0500

    first commit

--- app/routes.py
+++ app/routes.py
@@ -2,41 +2,81 @@ import os
 from app import app
 from flask import render_template, request, redirect
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
-
-
-# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name' 
+app.config['MONGO_DBNAME'] = 'database-name'
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri' 
+app.config['MONGO_URI'] = 'mongodb+srv://admin:hEzsvuNPe20opxCn@cluster0-js43a.mongodb.net/test?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
-
-# INDEX
+# # -------------------------------------------------------------------------------------------------------------------
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return render_template('index.html', events = events)
+    #connect to the Mongo DB
+    collection = mongo.db.events
+    files = list(collection.find({}))
+    print(files)
+    return render_template('index.html', files=files)
 
 
-# CONNECT TO DB, ADD DATA
+# ---------------------------------------------------------------------------------------------------------------------
 
-@app.route('/add')
+@app.route('/results', methods = ["get", "post"])
+def results():
 
-def add():
-    # connect to the database
+    user_info = dict(request.form)
+    print(user_info)
 
-    # insert new data
 
-    # return a message to the user
-    return ""
+    clientname = user_info["clientname"]
+    print("the client name is ", clientname)
+# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+    filename = user_info["filename"]
+    print("the file name is ", filename)
+
+# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    filelocation = user_info["filelocation"]
+    print("the file location is ", filelocation)
+# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    filelocation = user_info["filelocation"]
+    print("the file location is ", filelocation)
+# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    activity = user_info["activity"]
+    print("the clients status is ", activity)
+# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    creationdate = user_info["creationdate"]
+    print("the file was created on ", creationdate)
+# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+
+    collection = mongo.db.events
+
+    collection.insert({"clientname": clientname, "filename": filename, "filelocation": filelocation, "activity": activity, "creationdate": creationdate})
+
+# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+    return redirect('/index')
+
+
+    collection = mongo.db.events
+
+
+    return redirect('/index')
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+@app.route('/delete')
+def delete():
+    collection = mongo.db.events
+
+    collection.delete_many({})
+
+    return redirect('/index')
